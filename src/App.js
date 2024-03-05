dcfb5075a0a89e3b9fb0842675ed640282a81041
Joao Lara <joaovdelara@gmail.com>
2024-03-05 09:12:21 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function TodoList() {
+  const [inputValue, setInputValue] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (inputValue.trim() === '') {
+      return;
+    }
+
+    const newTask = {
+      task: inputValue,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setInputValue('');
+  };
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)];
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        className="input-task"
+        placeholder="Input an Item"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button className="button-task" onClick={addTask}>
+        <p>+</p>
+      </button>
+      <ul className="list-task">
+        {tasks.map((item, index) => (
+          <li key={index} className={`task ${item.completed ? 'done' : ''}`}>
+            <p>{item.task}</p>
+            <img
+              src="./img/trash.png"
+              alt="Delete-Tarefa"
+              onClick={() => deleteTask(index)}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
